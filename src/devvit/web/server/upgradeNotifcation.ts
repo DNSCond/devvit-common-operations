0f09d993f2df0759a2243efc28e6672116e2f866
qin2007 <qin@ractoc.com>
2025-11-06 11:13:09 +0100

    res.status(400).json({ status: 'Bad Request' });

--- src/devvit/web/server/upgradeNotifcation.ts
+++ src/devvit/web/server/upgradeNotifcation.ts
@@ -48,41 +48,46 @@ dependancies:
 
 */
 export function attachUpgradeNotifcationToRouter(router: Router, appDisplayName: string): Router {
-    router.post('/internal/scheduler/weekly-wednesday', async (_req: Request, res: Response) => {
-        const now = new Date;
-        console.log(`Handle weekly Wednesday 1pm task at ${now.toISOString()}!`);
-        if (await settings.get('upgradeNotifcation-sendUpgradeNotif')) {
-            const latest = await settings.get<string>('upgradeNotifcation-semver-latest'),
-                { subredditId, subredditName } = context;
-            if (!latest) {
-                console.error('latest is undefined;', latest);
-                return;
-            } else if (!/^\d+\.\d+\.\d+$/.test(latest)) {
-                console.error('the developer has set latest incorrectly; ', latest);
-                return;
-            }
-            if (!lt(context.appVersion, latest)) {
-                console.log("no update found.");
-                return;
-            }
+  router.post('/internal/scheduler/weekly-wednesday', async (_req: Request, res: Response) => {
+    const now = new Date;
+    console.log(`Handle weekly Wednesday 1pm task at ${now.toISOString()}!`);
+    if (await settings.get('upgradeNotifcation-sendUpgradeNotif')) {
+      const latest = await settings.get<string>('upgradeNotifcation-semver-latest'),
+        { subredditId, subredditName } = context;
+      if (!latest) {
+        console.error('latest is undefined;', latest);
+        res.status(400).json({ status: 'Bad Request' });
+        return;
+      } else if (!/^\d+\.\d+\.\d+$/.test(latest)) {
+        console.error('the developer has set latest incorrectly; ', latest);
+        res.status(400).json({ status: 'Bad Request' });
+        return;
+      }
+      if (!lt(context.appVersion, latest)) {
+        console.log("no update found.");
+        res.status(400).json({ status: 'Bad Request' });
+        return;
+      }
 
-            if ((await redis.get('upgradeNotifcation-notifSent')) === latest) {
-                console.log('maybe i have already sent the notif for', latest);
-                return;
-            }
-            let subject = `New "${appDisplayName}" Update is Available: ${latest};`, bodyMarkdown = subject +
-                `\n\nTo install this update, or to disable these notifications, visit the [**${appDisplayName} Configuration Page**](https://developers.reddit.com/r/${subredditName}/apps/${context.appName}) for /r/${subredditName}.`;
+      if ((await redis.get('upgradeNotifcation-notifSent')) === latest) {
+        console.log('maybe i have already sent the notif for', latest);
+        res.status(400).json({ status: 'Bad Request' });
+        return;
+      }
 
-            await reddit.modMail.createModNotification({
-                subredditId, subject, bodyMarkdown,
-            });
+      let subject = `New "${appDisplayName}" Update is Available: ${latest};`, bodyMarkdown = subject +
+        `\n\nTo install this update, or to disable these notifications, visit the [**${appDisplayName} Configuration Page**](https://developers.reddit.com/r/${subredditName}/apps/${context.appName}) for /r/${subredditName}.`;
 
-            // using utc explicitly, is better practice in my opinion/
-            now.setUTCDate(now.getUTCDate() + 90);
-            await redis.set('upgradeNotifcation-notifSent', latest, { expiration: now })
-        }
-        res.status(200).json({ status: 'ok' });
-    }); return router;
+      await reddit.modMail.createModNotification({
+        subredditId, subject, bodyMarkdown,
+      });
+
+      // using utc explicitly, is better practice in my opinion/
+      now.setUTCDate(now.getUTCDate() + 90);
+      await redis.set('upgradeNotifcation-notifSent', latest, { expiration: now })
+    }
+    res.status(200).json({ status: 'ok' });
+  }); return router;
 }
 
 // `u/antboiy <https://reddit.com/u/antboiy/>`;
